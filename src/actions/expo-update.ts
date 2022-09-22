ebbc404d944dca4b10de793ab5e9440204167ebe
Cristian Gatu <cristiangatu@CGatu-ROMAC21.local>
2022-09-23 00:24:05 +0300

    Add new command

---
+++ src/actions/expo-update.ts
@@ -0,0 +1,11 @@
+import { projectInfo, projectOwner } from '../expo';
+import { executeAction } from '../worker';
+import { CommentInput, commentInput } from './preview-comment';
+
+executeAction(async (input: CommentInput = commentInput()) => {
+  const project = await projectInfo(input.project);
+  if (!project.owner) {
+    project.owner = await projectOwner();
+  }
+  console.log(project);
+});
